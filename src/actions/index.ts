c737475ebb1e16bd6f13fd5584defd44f1b12180
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-29 16:55:56 -0300

    270 - Action Types Enum

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,20 +1,21 @@
 import axios, { AxiosResponse } from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-interface Response extends AxiosResponse {
-  data: [{ userId: number; id: number; title: string; completed: boolean }];
+interface Todo {
+  data: { userId: number; id: number; title: string; completed: boolean };
 }
 
 interface CustomDispatch extends Dispatch {
   type: string;
-  payload: Response;
+  payload: Todo;
 }
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response: Response = await axios.get(url);
-    dispatch({ type: 'FETCH_TODOS', payload: response.data });
+    const response = await axios.get<Todo[]>(url);
+    dispatch({ type: ActionTypes.fetchTodos, payload: response.data });
   };
 };
---
+++ src/actions/types.ts
@@ -0,0 +1,5 @@
+import { fetchTodos } from '.';
+
+export enum ActionTypes {
+  fetchTodos = 'FETCH_TODOS',
+}
