d7fa40ff05f939432a262e24f03e3ac981a2b4ff
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-29 16:33:12 -0300

    268 - Redux Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-interface AppProps {
-  color?: string;
-}
-interface AppState {
-  counter: number;
-}
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>Hello, functional component</div>;
-};
+const store = createStore(reducers, applyMiddleware(thunk));
 
-// class App extends React.Component<AppProps, AppState> {
-//   constructor(props: AppProps) {
-//     super(props);
-//     this.state = { counter: 0 };
-//   }
+ReactDOM.render(
+  <Provider store={store}>
+    <App color='red' />
+  </Provider>,
 
-//   onIncrement = () => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
-
-//   onDecrement = () => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
-
-ReactDOM.render(<App color='red' />, document.querySelector('#root'));
+  document.querySelector('#root')
+);
