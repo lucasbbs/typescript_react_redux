14322f38ec4cc7ffc8a0bab3df8f76c3ba78ad7f
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-29 15:59:36 -0300

    264 - Interfaces with Props

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+interface AppProps {
+  color?: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component<AppProps> {
+  render() {
+    return <div>{this.props.color}</div>;
+  }
+}
+
+ReactDOM.render(<App color='red' />, document.querySelector('#root'));
