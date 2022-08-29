9ad6bd4f75950f59a4b1054810efa40121ec73f5
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2022-08-29 17:39:34 -0300

    274 - Connecting a Component to Redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
 interface AppProps {
-  color?: string;
+  todos?: Todo[];
+  fetchTodos(): any;
 }
-interface AppState {
-  counter: number;
-}
+interface AppState {}
 
-export class App extends React.Component<AppProps, AppState> {
+class _App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = { counter: 0 };
   }
 
-  onIncrement = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = () => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
   render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
+    return <div>{console.log('Hello There')} </div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
